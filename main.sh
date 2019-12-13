ebd179893e98398a765382efbeda7578ea69dadb
Mikhail Chabanov <mchabanov@cyngn.com>
2019-12-12 17:20:18 -0800

    Add python3.7 instalation task

--- main.sh
+++ main.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-ansible-playbook -v --connection=local main.yml
+sudo echo 'Start system configuration'
+sudo /usr/bin/python2 /usr/local/bin/ansible-playbook -v --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
