09fb04cef5f5347106fa1c4f7cbda3c051db3d35
Mikhail Chabanov <mchabanov@cyngn.com>
2019-12-12 19:15:53 -0800

    Use alternatives to reference Python37

--- main.sh
+++ main.sh
@@ -2,13 +2,13 @@
 sudo echo 'Start system configuration'
 
 if [ -f "/usr/bin/ansible-playbook" ]; then
-	echo /usr/bin/ansible-playbook
+	echo Use /usr/bin/ansible-playbook
     sudo /usr/bin/python2 /usr/bin/ansible-playbook -v --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
     exit
 fi
 
 if [ -f "/usr/local/bin/ansible-playbook" ]; then
-	echo /usr/local/bin/ansible-playbook
+	echo Use /usr/local/bin/ansible-playbook
 	sudo /usr/bin/python2 /usr/local/bin/ansible-playbook -v --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
     exit
 fi
