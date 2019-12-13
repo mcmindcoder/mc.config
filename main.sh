6d026547689182430e30984666b871b1748f636a
Misha C <mchabanov@cyngn.com>
2019-12-12 18:15:04 -0800

    Support multiple ansible-plyabook locations

--- main.sh
+++ main.sh
@@ -1,3 +1,16 @@
 #/bin/sh
 sudo echo 'Start system configuration'
-sudo /usr/bin/python2 /usr/local/bin/ansible-playbook -v --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
+
+if [ -f "/usr/bin/ansible-playbook" ]; then
+	echo /usr/bin/ansible-playbook
+    sudo /usr/bin/python2 /usr/bin/ansible-playbook -v --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
+    exit
+fi
+
+if [ -f "/usr/local/bin/ansible-playbook" ]; then
+	echo /usr/local/bin/ansible-playbook
+	sudo /usr/bin/python2 /usr/local/bin/ansible-playbook -v --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
+    exit
+fi
+
+
