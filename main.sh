c9336fa63955216c262cf1ac7815fd4d9495f45a
mc <mc.mindcoder@gmail.com>
2020-12-30 20:17:33 -0800

    Define user automatically on start

--- main.sh
+++ main.sh
@@ -4,17 +4,17 @@ echo
 
 ARGS="$*"
 if [ -f "/usr/bin/ansible-playbook" ]; then
-	echo Use /usr/bin/ansible-playbook
-    sudo /usr/bin/python3 /usr/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python3'
-  exit
+	sudo echo Use /usr/bin/ansible-playbook
+	/usr/bin/python3 /usr/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python3'
+	exit
 elif [ -f "/usr/local/bin/ansible-playbook" ]; then
-	echo Use /usr/local/bin/ansible-playbook
-	sudo /usr/bin/python3 /usr/local/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python3'
-  exit
+	sudo echo Use /usr/local/bin/ansible-playbook
+	/usr/bin/python3 /usr/local/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python3'
+	exit
 else
-  echo 'Error: ansible is not installed' >&2
-  echo 'Run sudo apt install ansible' >&2
-  exit 1
+	echo 'Error: ansible is not installed' >&2
+	echo 'Run sudo apt install ansible' >&2
+	exit 1
 fi
 
 
