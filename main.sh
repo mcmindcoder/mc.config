e8aaa112a474bf8cb000deb39e6abbbd90d24422
mc <mc.mindcoder@gmail.com>
2020-12-30 17:49:24 -0800

    Switch to python3 used by default in mint30

--- main.sh
+++ main.sh
@@ -5,11 +5,11 @@ echo
 ARGS="$*"
 if [ -f "/usr/bin/ansible-playbook" ]; then
 	echo Use /usr/bin/ansible-playbook
-  sudo /usr/bin/python2 /usr/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
+    sudo /usr/bin/python3 /usr/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python3'
   exit
 elif [ -f "/usr/local/bin/ansible-playbook" ]; then
 	echo Use /usr/local/bin/ansible-playbook
-	sudo /usr/bin/python2 /usr/local/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python2'
+	sudo /usr/bin/python3 /usr/local/bin/ansible-playbook $ARGS --connection=local main.yml -e 'ansible_python_interpreter=/usr/bin/python3'
   exit
 else
   echo 'Error: ansible is not installed' >&2
